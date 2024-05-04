8177e139437c23bdf22da1e4a531c32fdef0df89
Thanh Nguyen <72685335+huscongao1692003@users.noreply.github.com>
2024-05-04 13:24:48 +0700

    Remove TranslucentBlur, add loading Login, add controller input

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -13,10 +13,17 @@ public class LoginManager : MonoBehaviour
     public string baseUrl = "https://appsoftspace.azurewebsites.net/Login/user";
     public TMP_InputField usernameInputField;
     public TMP_InputField passwordInputField;
+    public GameObject loadingPanel;
+    public GameObject errorPanel;
+    public Slider loadingSlider;
 
-  
+
+
     public void Login()
     {
+        loadingSlider.value = 10;
+        loadingPanel.SetActive(true);
+        
         Debug.Log(usernameInputField.text);
         Debug.Log(passwordInputField.text);
         var jsonBody = new UserLogin
@@ -31,16 +38,22 @@ public class LoginManager : MonoBehaviour
         {
             if (response.StatusCode == 200)
             {
+                loadingSlider.value = 100;
                 SceneManager.LoadScene("IdeaSceneVR", LoadSceneMode.Single);
+                loadingPanel.SetActive(false);
+               
             }
             else
             {
                 Debug.LogError("Login failed. Status code: " + response.StatusCode);
+                loadingPanel.SetActive(false);
+                errorPanel.SetActive(true);
 
             }
         }).Catch(error =>
         {
             Debug.LogError("Error: " + error.Message);
+            errorPanel.SetActive(true);
 
         });
 
